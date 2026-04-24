85785ee341e0c48d6c64ce57f81cf191713050e1
Arnaud 'red' Rouyer <joshleaves@users.noreply.github.com>
2026-04-24 02:14:20 +0200

    Update wherefrom.rb: Upgrade to v0.1.3

--- Formula/wherefrom.rb
+++ Formula/wherefrom.rb
@@ -1,16 +1,16 @@
 class Wherefrom < Formula
   desc "Read file origin from macOS metadata (kMDItemWhereFroms)"
   homepage "https://github.com/joshleaves/wherefrom"
-  version "0.1.1"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joshleaves/wherefrom/releases/download/v0.1.1/wherefrom-aarch64-apple-darwin"
-      sha256 "0016a6526b1135fb73cbbb66e68b2d1f493172ddfb698450daade38f63ba5a51"
+      url "https://github.com/joshleaves/wherefrom/releases/download/v0.1.3/wherefrom-aarch64-apple-darwin"
+      sha256 "8e02e2115afd72bb9b928fe4aa2e5de3e6c409a815b0cbd4d0ce3e42dc4c5b14"
     else
-      url "https://github.com/joshleaves/wherefrom/releases/download/v0.1.1/wherefrom-x86_64-apple-darwin"
-      sha256 "237a107e37b093c539fe893f5fb34764a6a41ff1c555091755c7c5bd1fb4abf9"
+      url "https://github.com/joshleaves/wherefrom/releases/download/v0.1.3/wherefrom-x86_64-apple-darwin"
+      sha256 "e56f46206e463bdd7b87a1eb4b9b06481079cb2f6123b8a5f230d830d4a9e49c"
     end
   end
 
